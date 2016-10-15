5e4ecd3d6898caf5d2c158c0faa2b3f30bb1b4c1
xiongkuang <xiongkuang@gmail.com>
2016-10-15 10:52:53 -0400

    added video pages

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';//equals to const Component = React.Component;
 
+// //function based component
 // const SearchBar = () => {
 //     return <input />;
 // }
 
 
-
+//class based component
 class SearchBar extends Component{
-    render(){
-        return <input />;
+    constructor(props){
+        super(props);
+
+        this.state = {term: 'Search'};
     }
+
+    render(){
+        // this.state.term = event.target.value;//BAD!!
+        return (
+            <div>
+                <input
+                    value = {this.state.term}
+                    onChange={event => this.setState({term: event.target.value})} />
+                {/*Value of the input : {this.state.term}*/}
+            </div>
+    )
+    }
+
+    // onInputChange(event){
+    //     console.log(event.target.value);
+    // }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCfPTV1qXHa1VnCXDOle9SKWwisf1ipyAk';
 
-//create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
-}
 
+//create a new component. This component should produce some HTML
+// const App = () => {
+//     return (
+//         <div>
+//             <SearchBar/>
+//         </div>
+//     );
+// }
+
+class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'rotk'}, (videos) => {
+            this.setState({ videos });
+            //this.setState({ videos : videos}); //if the key and the property has the same name
+        });
+
+    }
+
+    render(){
+        return(
+            <div>
+                <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos = {this.state.videos} />
+            </div>
+        );
+    }
+
+}
 
 
 //Take this component's generated HTML and put it on the page
